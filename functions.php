a0fd9d78bc83f20291356cc1a94407e62363962f
Freitas <RODRIGO.FREITAS@sicpa.com>
2020-04-22 15:22:14 -0300

    HTML -> WP
    - Resolved CSS path error
    - Created 'presentation' post type
    - Puted 'presentation' post on home page

--- functions.php
+++ functions.php
@@ -4,18 +4,18 @@
 
     function rdfreitas_add_script_header(){
 
-        wp_enqueue_style('rdfreitas_iconic-bootstrap', get_stylesheet_directory_uri().'css/open-iconic-bootstrap.min.css');
-        wp_enqueue_style('rdfreitas_animate', get_stylesheet_directory_uri().'css/animate.css');
-        wp_enqueue_style('rdfreitas_owl-carousel', get_stylesheet_directory_uri().'css/owl.carousel.min.css');
-        wp_enqueue_style('rdfreitas_owl-theme', get_stylesheet_directory_uri().'css/owl.theme.default.min.css');
-        wp_enqueue_style('rdfreitas_magnific-popup', get_stylesheet_directory_uri().'css/magnific-popup.css');
-        wp_enqueue_style('rdfreitas_aos', get_stylesheet_directory_uri().'css/aos.css');
-        wp_enqueue_style('rdfreitas_ionicons', get_stylesheet_directory_uri().'css/ionicons.min.css');
-        wp_enqueue_style('rdfreitas_datepicker', get_stylesheet_directory_uri().'css/bootstrap-datepicker.css');
-        wp_enqueue_style('rdfreitas_timepicker', get_stylesheet_directory_uri().'css/jquery.timepicker.css');
-        wp_enqueue_style('rdfreitas_flaticon', get_stylesheet_directory_uri().'css/flaticon.css');
-        wp_enqueue_style('rdfreitas_icomoon', get_stylesheet_directory_uri().'css/icomoon.css');
-        wp_enqueue_style('rdfreitas_style'), get_stylesheet_directory_uri().'css/style.css');
+        wp_enqueue_style('rdfreitas_iconic-bootstrap', get_stylesheet_directory_uri().'/css/open-iconic-bootstrap.min.css');
+        wp_enqueue_style('rdfreitas_animate', get_stylesheet_directory_uri().'/css/animate.css');
+        wp_enqueue_style('rdfreitas_owl-carousel', get_stylesheet_directory_uri().'/css/owl.carousel.min.css');
+        wp_enqueue_style('rdfreitas_owl-theme', get_stylesheet_directory_uri().'/css/owl.theme.default.min.css');
+        wp_enqueue_style('rdfreitas_magnific-popup', get_stylesheet_directory_uri().'/css/magnific-popup.css');
+        wp_enqueue_style('rdfreitas_aos', get_stylesheet_directory_uri().'/css/aos.css');
+        wp_enqueue_style('rdfreitas_ionicons', get_stylesheet_directory_uri().'/css/ionicons.min.css');
+        wp_enqueue_style('rdfreitas_datepicker', get_stylesheet_directory_uri().'/css/bootstrap-datepicker.css');
+        wp_enqueue_style('rdfreitas_timepicker', get_stylesheet_directory_uri().'/css/jquery.timepicker.css');
+        wp_enqueue_style('rdfreitas_flaticon', get_stylesheet_directory_uri().'/css/flaticon.css');
+        wp_enqueue_style('rdfreitas_icomoon', get_stylesheet_directory_uri().'/css/icomoon.css');
+        wp_enqueue_style('rdfreitas_style', get_stylesheet_directory_uri().'/css/style.css');
     
     }
 
@@ -32,9 +32,10 @@
         wp_enqueue_script('rdfreitas_aos-js', get_stylesheet_directory_uri() . '/js/aos.js');
         wp_enqueue_script('rdfreitas_jquery-animateNumber-js', get_stylesheet_directory_uri() . '/js/jquery.animateNumber.min.js');
         wp_enqueue_script('rdfreitas_datepicker-js', get_stylesheet_directory_uri() . '/js/bootstrap-datepicker.js');
-        wp_enqueue_script('rdfreitas_', get_stylesheet_directory_uri() . '/js/jquery.timepicker.min.js');
         wp_enqueue_script('rdfreitas_scrollax-js', get_stylesheet_directory_uri() . '/js/scrollax.min.js');
         wp_enqueue_script('rdfreitas_main-js', get_stylesheet_directory_uri() . '/js/main.js');
     }
+
+    require_once('post-presentation.php');
 
 ?>
