c3d337d9ea3ab5724d0bdd948ad7060f157b8bbb
Freitas <RODRIGO.FREITAS@sicpa.com>
2020-04-22 10:45:49 -0300

    WP - Header and Footer
    - Files to create Wordpress Theme
    - Re-created footer.php
    - Create functions.php for calls to CSS and script files

---
+++ functions.php
@@ -0,0 +1,40 @@
+<?
+    add_action('wp_enqueue_scripts', 'rdfreitas_add_script_header');
+    add_action('wp_footer', 'rdfreitas_add_script_footer');
+
+    function rdfreitas_add_script_header(){
+
+        wp_enqueue_style('rdfreitas_iconic-bootstrap', get_stylesheet_directory_uri().'css/open-iconic-bootstrap.min.css');
+        wp_enqueue_style('rdfreitas_animate', get_stylesheet_directory_uri().'css/animate.css');
+        wp_enqueue_style('rdfreitas_owl-carousel', get_stylesheet_directory_uri().'css/owl.carousel.min.css');
+        wp_enqueue_style('rdfreitas_owl-theme', get_stylesheet_directory_uri().'css/owl.theme.default.min.css');
+        wp_enqueue_style('rdfreitas_magnific-popup', get_stylesheet_directory_uri().'css/magnific-popup.css');
+        wp_enqueue_style('rdfreitas_aos', get_stylesheet_directory_uri().'css/aos.css');
+        wp_enqueue_style('rdfreitas_ionicons', get_stylesheet_directory_uri().'css/ionicons.min.css');
+        wp_enqueue_style('rdfreitas_datepicker', get_stylesheet_directory_uri().'css/bootstrap-datepicker.css');
+        wp_enqueue_style('rdfreitas_timepicker', get_stylesheet_directory_uri().'css/jquery.timepicker.css');
+        wp_enqueue_style('rdfreitas_flaticon', get_stylesheet_directory_uri().'css/flaticon.css');
+        wp_enqueue_style('rdfreitas_icomoon', get_stylesheet_directory_uri().'css/icomoon.css');
+        wp_enqueue_style('rdfreitas_style'), get_stylesheet_directory_uri().'css/style.css');
+    
+    }
+
+    function rdfreitas_add_script_footer(){
+        wp_enqueue_script('rdfreitas_jquery-js', get_stylesheet_directory_uri() . '/js/jquery.min.js');
+        wp_enqueue_script('rdfreitas_jquery-migrate-js', get_stylesheet_directory_uri() . '/js/jquery-migrate-3.0.1.min.js');
+        wp_enqueue_script('rdfreitas_popper-js', get_stylesheet_directory_uri() . '/js/popper.min.js');
+        wp_enqueue_script('rdfreitas_bootstrap-js', get_stylesheet_directory_uri() . '/js/bootstrap.min.js');
+        wp_enqueue_script('rdfreitas_easing-js', get_stylesheet_directory_uri() . '/js/jquery.easing.1.3.js');
+        wp_enqueue_script('rdfreitas_jquery-waypoints-js', get_stylesheet_directory_uri() . '/js/jquery.waypoints.min.js');
+        wp_enqueue_script('rdfreitas_jquery-stellar-js', get_stylesheet_directory_uri() . '/js/jquery.stellar.min.js');
+        wp_enqueue_script('rdfreitas_owl-carousel-js', get_stylesheet_directory_uri() . '/js/owl.carousel.min.js');
+        wp_enqueue_script('rdfreitas_magnific-popup-js', get_stylesheet_directory_uri() . '/js/jquery.magnific-popup.min.js');
+        wp_enqueue_script('rdfreitas_aos-js', get_stylesheet_directory_uri() . '/js/aos.js');
+        wp_enqueue_script('rdfreitas_jquery-animateNumber-js', get_stylesheet_directory_uri() . '/js/jquery.animateNumber.min.js');
+        wp_enqueue_script('rdfreitas_datepicker-js', get_stylesheet_directory_uri() . '/js/bootstrap-datepicker.js');
+        wp_enqueue_script('rdfreitas_', get_stylesheet_directory_uri() . '/js/jquery.timepicker.min.js');
+        wp_enqueue_script('rdfreitas_scrollax-js', get_stylesheet_directory_uri() . '/js/scrollax.min.js');
+        wp_enqueue_script('rdfreitas_main-js', get_stylesheet_directory_uri() . '/js/main.js');
+    }
+
+?>
--- header.php
+++ header.php
@@ -8,24 +8,7 @@
     <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800,900" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Lora:400,400i,700,700i" rel="stylesheet">
 
-    <link rel="stylesheet" href="css/open-iconic-bootstrap.min.css">
-    <link rel="stylesheet" href="css/animate.css">
-    
-    <link rel="stylesheet" href="css/owl.carousel.min.css">
-    <link rel="stylesheet" href="css/owl.theme.default.min.css">
-    <link rel="stylesheet" href="css/magnific-popup.css">
-
-    <link rel="stylesheet" href="css/aos.css">
-
-    <link rel="stylesheet" href="css/ionicons.min.css">
-
-    <link rel="stylesheet" href="css/bootstrap-datepicker.css">
-    <link rel="stylesheet" href="css/jquery.timepicker.css">
-
-    
-    <link rel="stylesheet" href="css/flaticon.css">
-    <link rel="stylesheet" href="css/icomoon.css">
-    <link rel="stylesheet" href="css/style.css">
+    <? wp_head(); ?>
   </head>
   <body>
     
